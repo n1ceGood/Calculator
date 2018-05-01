b68d917c5da76c817a07b2aca4e9986d4d98d02b
n1ceGood <38880231+n1ceGood@users.noreply.github.com>
2018-05-01 16:09:59 +0300

    Test1 (#3)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,41 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("A");
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("");
+        System.out.println("Enter a");
+        Integer a = in.nextInt();
+        System.out.println("Enter b");
+        Integer b = in.nextInt();
+        System.out.println("What do you want to do"+"\n"+" Enter '1' = '+';"+"\n"+" Enter '2' = '-';"+"\n"+" Enter '3' = '*';"+"\n"+" Enter '4' = '/';");
+        Integer d = in.nextInt();
+        Integer  c ;
+        switch (d){
+            case 1:
+                c = a+b;
+                System.out.println(a+"+"+b+"="+c);
+                break;
+            case 2:
+                c=a-b;
+                System.out.println(a+"-"+b+"="+c);
+                break;
+            case 3:
+                c=a*b;
+                System.out.println(a+"*"+b+"="+c);
+                break;
+            case 4:
+                c=a/b;
+                System.out.println(a+"/"+b+"="+c);
+                break;
+            default:
+                System.out.println("You did not select an action");
+        }
     }
 }
+
+
