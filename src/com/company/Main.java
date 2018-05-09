bca2d9d22bd215a878aeb97d0098a881ceb7ea37
Kostia <adaw36909@gmail.com>
2018-05-09 21:19:04 +0300

    update (#5)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,41 +1,51 @@
 package com.company;
 
+import java.awt.*;
 import java.util.Scanner;
+import java.util.concurrent.ScheduledExecutorService;
 
 public class Main {
 
     public static void main(String[] args) {
+       Scanner in = new Scanner(System.in);
 
-        Scanner in = new Scanner(System.in);
-        System.out.println("");
-        System.out.println("Enter a");
-        Integer a = in.nextInt();
+
+                System.out.println("Enter a");
+        double a = in.nextDouble();
         System.out.println("Enter b");
-        Integer b = in.nextInt();
-        System.out.println("What do you want to do"+"\n"+" Enter '1' = '+';"+"\n"+" Enter '2' = '-';"+"\n"+" Enter '3' = '*';"+"\n"+" Enter '4' = '/';");
-        Integer d = in.nextInt();
-        Integer  c ;
-        switch (d){
-            case 1:
-                c = a+b;
-                System.out.println(a+"+"+b+"="+c);
-                break;
-            case 2:
-                c=a-b;
-                System.out.println(a+"-"+b+"="+c);
-                break;
-            case 3:
-                c=a*b;
-                System.out.println(a+"*"+b+"="+c);
-                break;
-            case 4:
-                c=a/b;
-                System.out.println(a+"/"+b+"="+c);
-                break;
-            default:
-                System.out.println("You did not select an action");
-        }
+            double b = in.nextInt();
+            System.out.println("What do you want to do" + "\n" + " Enter '+';" + "\n" + " Enter '-';" + "\n" + " Enter '*';" + "\n" + " Enter '/';");
+            double c;
+
+            String d = in.next();
+
+            switch (d) {
+                case "+":
+                    c = a + b;
+                    System.out.println(a + "+" + b + "=" + c);
+                    break;
+                case "-":
+                    c = a - b;
+                    System.out.println(a + "-" + b + "=" + c);
+                    break;
+                case "*":
+                    c = a * b;
+                    System.out.println(a + "*" + b + "=" + c);
+                    break;
+                case "/":
+                    if (b == 0) {
+                        System.out.println("На ноль делить нельзя");
+                    } else {
+                        c = a / b;
+                        System.out.println(c);
+                    }
+                    break;
+                default:
+                    System.out.println("You did not select an action");
+            }
+
+
+
+
     }
 }
-
-
